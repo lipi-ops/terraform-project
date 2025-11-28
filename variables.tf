a26645795e13e86ca488bd1e702054e15f711419
lipi-ops <raylipi@gmail.com>
2025-11-28 15:47:22 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+ariable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "AWS secret key"
+  type        = string
+}
