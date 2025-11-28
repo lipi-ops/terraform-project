4ea2d2274d0783a7bbb2c6ee35937e9d8020e9b4
lipi-ops <raylipi@gmail.com>
2025-11-28 20:10:39 +0530

    Delete modules/ec2 directory

--- modules/ec2/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "myserver" {
-  ami = "ami-0d176f79571d18a8f"
-  instance_type = "t2.micro"
-  availability_zone = "ap-south-1a"
-  tags = {
-       Name = "myserver"
-   }
-}
