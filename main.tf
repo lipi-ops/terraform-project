1b51bdb149b9669d7e72a10268009ed0c846377d
lipi-ops <raylipi@gmail.com>
2025-11-28 21:08:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
 provider "aws" {
   region = var.region
-  }
+}
 
 module "network" {
-  source="./modules/network/"
+  source = "./modules/network"
+  region = var.region
 }
 module "ec2-instances" {
   source="./modules/ec2-instances/"
