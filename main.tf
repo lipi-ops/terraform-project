d7fb173ded3f2c6dbac5493b0dcb472cd5b7f598
lipi-ops <raylipi@gmail.com>
2025-11-28 20:53:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,10 @@ provider "aws" {
 
 module "network" {
   source="./modules/network/"
+region = var.region
 }
 module "ec2-instances" {
   source="./modules/ec2-instances/"
+region = var.region
  
  }
