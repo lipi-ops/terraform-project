4ad08b44ec7b063fee90d59b5ed3b80172cfd3bb
lipi-ops <raylipi@gmail.com>
2025-11-28 17:24:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,4 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
-}
+variable "region" {}
+variable "access_key" {}
+variable "secret_key" {}
 
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS access key"
-  type        = string
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS secret key"
-  type        = string
-}
